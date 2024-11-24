d9379d278ee08ed0cacb8cbf02c9ee88a3c82a0d
MirzaOrozco <mirza.orozco-alcantara@student.nbi-handelsakademin.se>
2024-11-24 23:30:50 +0100

    Added Author commands, handlers and tests

---
+++ Application/Commands/Authors/CreateAuthorCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Authors
+{
+    public class CreateAuthorCommand
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Presentation/Controllers/AuthorController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.Commands.Authors;
+using Infrastructure.Data;
+using Application.Handler;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly FakeDatabase _db;
+
+        public AuthorController()
+        {
+            _db = new FakeDatabase();
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthor([FromBody] CreateAuthorCommand command)
+        {
+            var handler = new CreateAuthorCommandHandler(_db);
+            try
+            {
+                handler.Handle(command);
+                return Ok("Author created succesfully.");
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateAuthor(int id, [FromBody] UpdateAuthorCommand command)
+        {
+            command.Id = id;
+            var handler = new UpdateAuthorCommandHandler(_db);
+            try
+            {
+                handler.Handle(command);
+                return Ok("Author update succesfully.");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAuthor(int id)
+        {
+            var command = new DeleteAuthorCommand { Id = id };
+            var handler = new DeleteAuthorCommandHandler(_db);
+            try
+            {
+                handler.Handle(command);
+                return Ok("Author deleted succesfully");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
